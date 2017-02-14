aee0579a05ddd51d9a7d71779db9293a43170092
rafen <capdevielle@gmail.com>
2017-02-14 09:40:04 -0300

    Added setup installation script

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-aristotle-indicators',
+    version='0.1',
+    packages=['indicators'],
+    include_package_data=True,
+    license='BSD Licence',
+    description='',
+    long_description=README,
+    url='https://github.com/LogicalOutcomes/aristotle-indicators',
+    author='Rafael Capdevielle',
+    author_email='rafael@logicaloutcomes.net',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'aristotle-metadata-registry',
+    ]
+)
