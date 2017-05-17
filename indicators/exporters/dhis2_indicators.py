ff6ac1f118f246c5813920d24ac0192de5fd783d
rafen <capdevielle@gmail.com>
2017-05-17 11:34:36 -0300

    Added Data Elements and Indicators export for DHIS2

--- indicators/exporters/dhis2_indicators.py
+++ indicators/exporters/dhis2_indicators.py
@@ -1,7 +1,12 @@
 import requests
-from aristotle_mdr.models import ValueDomain
+from aristotle_mdr.models import ValueDomain, DataElement
+from comet.models import Indicator
 from django.utils.text import slugify
 from ..models import Category, CategoryOption, CategoryCombination
+
+
+class DHIS2ClientException(Exception):
+    pass
 
 
 class DHIS2Client(object):
@@ -11,18 +16,12 @@ class DHIS2Client(object):
         'paging': 'false',
     }
 
-    endpoints = {
-        'categoryOptions': '/api/categoryOptions',
-        'categories': '/api/categories',
-        'categoryCombos': '/api/categoryCombos',
-        'optionSets': '/api/optionSets',
-        'options': '/api/options',
-    }
-
-    def __init__(self, server_url, user, password):
+    def __init__(self, server_url, user, password, version=25):
         self.server_url = server_url.rstrip('/')
+        self.version = version
         self.user = user
         self.password = password
+        self.api_url = '{}/api/{}/'.format(self.server_url, self.version)
 
     # Basic calls
     def _get(self, url, params=None):
@@ -41,14 +40,14 @@ class DHIS2Client(object):
 
     # Schemas
     def get(self, endpoint, obj_id=None, params=None):
-        url = self.server_url + self.endpoints[endpoint]
+        url = self.api_url + endpoint
         if obj_id:
             url += '/{}'.format(obj_id)
         response = self._get(url, params=params)
         return response.json()
 
     def post(self, endpoint, data, obj_id=None):
-        url = self.server_url + self.endpoints[endpoint]
+        url = self.api_url + endpoint
         if obj_id:
             url += '/{}'.format(obj_id)
         response = self._post(url, json=data, params=self.params)
@@ -62,7 +61,11 @@ class DHIS2Client(object):
         return None
 
     def create_element(self, collection_name, data):
-        return self.post(collection_name, data)
+        res = self.post(collection_name, data)
+        # raise if any error was found
+        if res['status'] in ['WARNING', 'ERROR']:
+            raise DHIS2ClientException('Error: {}'.format(res))
+        return res
 
     def get_or_create_element(self, collection_name, data, lookup_field='name'):
         created = False
@@ -90,8 +93,12 @@ class DHIS2Exporter(object):
         )
 
     def export(self):
-        val_dom = ValueDomain.objects.get(pk=2)
-        self.export_option_set(val_dom)
+        indicator = Indicator.objects.get(id=20)
+        self.export_indicator(indicator)
+        # data_element = DataElement.objects.get(pk=19)
+        # self.export_data_element(data_element)
+        # val_dom = ValueDomain.objects.get(pk=2)
+        # self.export_option_set(val_dom)
         # comb = CategoryCombination.objects.get(id=2)
         # self.export_category_combination(comb)
 
@@ -99,7 +106,7 @@ class DHIS2Exporter(object):
         opt, c = self.dhis2.get_or_create_element('categoryOptions', {
             'name': obj.name, 'shortName': obj.short_name, 'code': obj.code
         })
-        return opt['id']
+        return opt
 
     def export_category(self, obj):
         cat, c = self.dhis2.get_or_create_element('categories', {
@@ -107,9 +114,9 @@ class DHIS2Exporter(object):
             'dataDimensionType': 'DISAGGREGATION'
         })
         for option in obj.options.all():
-            opt_id = self.export_category_options(option)
-            self.dhis2.add_to_collection('categories', cat['id'], 'categoryOptions', opt_id)
-        return cat['id']
+            res_opt = self.export_category_options(option)
+            self.dhis2.add_to_collection('categories', cat['id'], 'categoryOptions', res_opt['id'])
+        return cat
 
     def export_category_combination(self, obj):
         comb, c = self.dhis2.get_or_create_element('categoryCombos', {
@@ -117,15 +124,15 @@ class DHIS2Exporter(object):
             'dataDimensionType': 'DISAGGREGATION'
         })
         for cat in obj.categories.all():
-            cat_id = self.export_category(cat)
-            self.dhis2.add_to_collection('categoryCombos', comb['id'], 'categories', cat_id)
-        return comb['id']
+            res_cat = self.export_category(cat)
+            self.dhis2.add_to_collection('categoryCombos', comb['id'], 'categories', res_cat['id'])
+        return comb
 
     def export_option_set_value(self, obj):
         val, c = self.dhis2.get_or_create_element('options', {
             'name': obj.meaning, 'code': obj.value
         })
-        return val['id']
+        return val
 
     def export_option_set(self, obj):
         opt, c = self.dhis2.get_or_create_element('optionSets', {
@@ -133,15 +140,89 @@ class DHIS2Exporter(object):
             'valueType': "TEXT"
         })
         for val in obj.permissiblevalue_set.all():
-            val_id = self.export_option_set_value(val)
-            self.dhis2.add_to_collection('optionSets', opt['id'], 'options', val_id)
-        return opt['id']
+            res_val = self.export_option_set_value(val)
+            self.dhis2.add_to_collection('optionSets', opt['id'], 'options', res_val['id'])
+        return opt
 
+    def export_data_element(self, obj):
+        # Add category combination
+        cat_combo_code = self.get_single_value_from_slot(obj, 'Category combination Code')
+        if cat_combo_code:
+            cat_combo_obj = CategoryCombination.objects.filter(code=cat_combo_code).first()
+            if cat_combo_obj:
+                category_combo = self.export_category_combination(cat_combo_obj)
+        # Default None category combo
+        if not category_combo:
+            category_combo = self.dhis2.get_element('categoryCombos', 'default')
+
+        # Add option set
+        option_set = None
+        if obj.valueDomain:
+            option_set = self.export_option_set(obj.valueDomain)
+
+        # Add Data Element
+        dt, c = self.dhis2.get_or_create_element('dataElements', {
+            'name': obj.name, 'shortName': obj.short_name,
+            'code': self.get_code_from_identifier(obj),
+            'description': obj.definition,
+            'formName': self.get_single_value_from_slot(obj, 'Form name', default=obj.name),
+            'domainType': self.get_single_value_from_slot(obj, 'Domain type'),
+            'valueType': self.get_single_value_from_slot(obj, 'Value type'),
+            'aggregationType': self.get_single_value_from_slot(obj, 'Aggregation operator'),
+            'categoryCombo': self.get_simple_repr(category_combo),
+            'optionSet': self.get_simple_repr(option_set, keys=['displayName', 'name', 'id', 'valueType']),
+        })
+        return dt
+
+    def export_indicator(self, obj):
+        # Generate numerators
+        numerator = obj.numerator_computation
+        numerators = {}
+        for num in obj.numerators.all():
+            res_num = self.export_data_element(num)
+            code = self.get_code_from_identifier(num)
+            numerators[code] = res_num
+            numerator = numerator.replace(code, '#{{{}}}'.format(res_num['id']))
+
+        # Generate denominators
+        denominator = obj.denominator_computation
+        denominators = {}
+        for den in obj.denominators.all():
+            res_den = self.export_data_element(den)
+            code = self.get_code_from_identifier(den)
+            denominators[code] = res_den
+            denominator = denominator.replace(code, '#{{{}}}'.format(res_num['id']))
+
+        # Indicator Type
+        ind_type = self.dhis2.get_element('indicatorTypes', 'Number')
+
+        # Generate Indicator
+        ind, c = self.dhis2.get_or_create_element('indicators', {
+            'name': obj.name, 'shortName': obj.short_name,
+            'code': self.get_code_from_identifier(obj),
+            'description': obj.definition,
+            'indicatorType': self.get_simple_repr(ind_type),
+            'numeratorDescription': obj.numerator_description,
+            'denominatorDescription': obj.denominator_description,
+            'numerator': numerator,
+            'denominator': denominator,
+        })
+        return ind
+
+    # Helpers
     def get_code_from_identifier(self, obj):
         ident = obj.identifiers.first()
         if ident:
             return ident.identifier
         else:
             return slugify(obj.name)
+
+    def get_single_value_from_slot(self, obj, slot_name, default=''):
+        slot = obj.slots.filter(name=slot_name).first()
+        return slot.value if slot else default
+
+    def get_simple_repr(self, data, keys=['displayName', 'name', 'id']):
+        return {k: v for k, v in data.items() if k in keys}
+
 
 exporter = DHIS2Exporter()
